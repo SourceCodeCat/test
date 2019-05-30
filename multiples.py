35b74391b435cc648f33f6a3365ccdc398810135
Marco Antonio Salgado Martinez <ing.marco@gmail.com>
2019-05-29 23:35:35 -0500

    updated multiples.py
    modified the functions so we can call them from unittest program

--- multiples.py
+++ multiples.py
@@ -24,28 +24,42 @@ msgs = {3:"Linio",5:"IT",8:"Linianos"}
 # This function claculates the possible key of the dict
 # From where we are going to retrieve the message
 def calc_key_component(d_r):
-    try:
-        # if the remainder is zero then we will get a ZeroDivisionError
-        return (d_r[1]*d_r[0])/d_r[1]
-    except ZeroDivisionError:
-        # we found a multiple of 3 or 5
-        return 0
+	try:
+		# if the remainder is zero then we will get a ZeroDivisionError
+		return (d_r[1]*d_r[0])/d_r[1]
+	except ZeroDivisionError:
+		# we found a multiple of 3 or 5
+		return 0
 
-    
-for n in range(1,101):
-    #create a tuple with the multilple and the reminder of n%multiple
-    d_r_3 = (3,n%3)
-    #create a tuple with the multilple and the reminder of n%multiple
-    d_r_5 = (5,n%5)
-    # calculate the key 
-    k = (8 - int(calc_key_component(d_r_3)+calc_key_component(d_r_5))) 
 
-    # we have the constraint of using only 1 'if'...so save the default value 
-    msg = n
-    if k in msgs:
-        #IF the key is in the dictionary then retrieve the correct message for the key
-        # and replace msg with the custom message...
-        msg = msgs[k]
-    
+def modifyMultiples(custom_range = None):
+	#This method will print  the numbers that are not multiple of 3,5 or both, if the number
+	#is a multiple of any of the above numbers the will repalce the number like this:
+	#multiple of 3 = Linio, multiples of 5= IT or if number is multiple of 3 and 5 = Linianos
 
-    print(msg)
+	#this variable is meant to be used by the unittest
+	multiples = []
+	for n in range(1,101) if custom_range == None else custom_range:
+		#create a tuple with the multilple and the reminder of n%multiple
+		d_r_3 = (3,n%3)
+		#create a tuple with the multilple and the reminder of n%multiple
+		d_r_5 = (5,n%5)
+		# calculate the key 
+		k = (8 - int(calc_key_component(d_r_3)+calc_key_component(d_r_5))) 
+
+		try:
+			#IF the key is in the dictionary then retrieve the correct message for the key
+			msg = msgs[k]
+		except KeyError:
+			# else use the actual digit
+			msg = n
+		
+		multiples.append(msg)	
+		#print(msg)
+	#return the multiples list, this is meant to be used by the unittest
+	return multiples
+
+
+
+if __name__ == '__main__':
+	print(modifyMultiples())
